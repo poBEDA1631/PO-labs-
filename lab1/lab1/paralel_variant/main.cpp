c2fbc7ecf5cecb01b5855d32ef60cc385f788247
poBEDA1631 <08062006kiril@gmail.com>
2026-02-08 19:40:49 +0200

    Додав рішення з паралелізацією

--- lab1/lab1/paralel_variant/main.cpp
+++ lab1/lab1/paralel_variant/main.cpp
@@ -1,23 +1,33 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <ctime>
 #include <algorithm>
 #include <chrono>
+#include <thread>
 
 using namespace std;
 
+void fill_diagonal(int start_row, int end_row, vector<vector<int>>& quadratic_matrix) {
+	for (int i = start_row; i < end_row; i++) {
+		auto maxInRow = *max_element(quadratic_matrix[i].begin(), quadratic_matrix[i].end());
+		quadratic_matrix[i][i] = maxInRow;
+	}
+}
+
 int main() {
 	srand(time(0));
-	int N = 5;
+	int N = 100;
+
 	vector<vector<int>> quadratic_matrix(N, vector<int>(N, 0));
-
-
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++)
 		{
 			quadratic_matrix[i][j] = rand() % 10 + 1;
 		}
 	}
+
+	int num_thread = 5;
+	vector<thread> threads;
 
 	volatile long long warmUpSum = 0;
 	for (int i = 0; i < N; i++) {
@@ -26,9 +36,14 @@ int main() {
 
 	auto start = chrono::high_resolution_clock::now();
 
-	for (int i = 0; i < N; i++) {
-		int maxInRow = *max_element(quadratic_matrix[i].begin(), quadratic_matrix[i].end());
-		quadratic_matrix[i][i] = maxInRow;
+	for (int i = 0; i < num_thread; i++) {
+		int start_row = i * N / num_thread;
+		int end_row = (i + 1) * N / num_thread;
+		threads.emplace_back(fill_diagonal, start_row, end_row, ref(quadratic_matrix));
+	}
+
+	for (auto& t : threads) {
+		t.join();
 	}
 
 	auto end = chrono::high_resolution_clock::now();
