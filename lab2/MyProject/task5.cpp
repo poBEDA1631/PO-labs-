479dc218044464ba5f90c9745df2e43d0d523004
poBEDA1631 <08062006kiril@gmail.com>
2026-02-28 19:30:32 +0200

    таска 5

---
+++ lab2/MyProject/task5.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <algorithm>
+#include <chrono>
+#include <iomanip>
+#include <thread>
+
+
+using namespace std;
+volatile int result = 0;
+
+
+void xor_sum_calc(int start, int end, const vector<int>& data) {
+	int local_sum = 0;
+	for (int i = start; i < end; i++) {
+		if (data[i] % 2 != 0)
+		{
+			local_sum = local_sum ^ data[i];
+		}
+	}
+
+	int old_val, new_val;
+	do {
+		old_val = result;
+		new_val = old_val ^ local_sum;
+	} while (__sync_val_compare_and_swap(&result, old_val, new_val) != old_val);
+}
+
+int main() {
+	srand(time(0));
+	int N = 10;
+	vector<int> data(N);
+
+	int num_thread = 10;
+	vector<thread> threads;
+
+	for (int i = 0; i < N; i++) {
+		data[i] = rand() % 8;
+	}
+
+	cout << "--- data (first 10 elements) ---" << endl;
+	for (int i = 0; i < (N > 10 ? 10 : N); i++) {
+		cout << setw(4) << data[i];
+	}
+	cout << "-----------------------------" << endl;
+
+	volatile long long warmUpSum = 0;
+	for (int i = 0; i < N; i++) {
+		warmUpSum += data[i];
+	}
+
+	auto start = chrono::high_resolution_clock::now();
+	for (int i = 0; i < num_thread; i++) {
+		int start_section = i * N / num_thread;
+		int end_section = (i + 1) * N / num_thread;
+		threads.emplace_back(xor_sum_calc, start_section, end_section, ref(data));
+	}
+
+	for (auto& t : threads) {
+		t.join();
+	}
+	auto end = chrono::high_resolution_clock::now();
+
+	chrono::duration<double, milli> elapsed = end - start;
+
+	cout << "Dimension of matrix: " << N << endl;
+	cout << "Warm-up sum: " << warmUpSum << endl;
+	cout << "Time:" << elapsed.count() << "ms" << endl;
+	cout << "Result:" << result << endl;
+
+	return 0;
+}
