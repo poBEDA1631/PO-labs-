7055415019653be94949cd76a3ef2a10bfafb236
poBEDA1631 <08062006kiril@gmail.com>
2026-03-15 17:55:08 +0200

    structure Task and class ThreadPool(add task to worksers)

--- lab3/lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3/lab3.cpp
@@ -1,12 +1,44 @@
-﻿// lab3.cpp: определяет точку входа для приложения.
-//
-
-#include "lab3.h"
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <chrono>
+#include <atomic>
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
-}
+struct Task {
+    function<void()> work;
+    int duration;
+    int id;
+};
+
+class ThreadPool {
+protected:
+    vector<thread> workers;
+    queue<Task> tasks;
+    mutex mtx;
+    condition_variable cv;
+    int total_queued_time = 0;
+
+public:
+    bool enqueue(int id, int duration) {
+        lock_guard<mutex> lock(mtx);
+        if (total_queued_time + duration > 60) {
+            return false;
+        }
+        Task new_task;
+        new_task.id = id;
+        new_task.duration = duration;
+        new_task.work = [duration]() {
+            this_thread::sleep_for(chrono::seconds(duration));
+            };
+        tasks.push(new_task);
+        total_queued_time += duration;
+        cv.notify_one();
+        return true;
+    }
+};
