97338c90431884b1219f89b1a0e87d13979b4107
poBEDA1631 <08062006kiril@gmail.com>
2026-03-16 01:20:12 +0200

    addting others methods

--- lab3/lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3/lab3.cpp
@@ -7,6 +7,8 @@
 #include <functional>
 #include <chrono>
 #include <atomic>
+#include <random>
+#include <iomanip>
 
 using namespace std;
 
@@ -26,9 +28,13 @@ private:
     bool stop_graceful = false;
     bool stop_immediate = false;
     bool is_paused = false;
+    atomic<int> rejected_tasks{ 0 };
+    atomic<int> completed_tasks{ 0 };
+    vector<chrono::duration<double>> worker_wait_times;
 
 public:
     ThreadPool(size_t threads_count) {
+        worker_wait_times.resize(threads_count, chrono::duration<double>(0));
         for (size_t i = 0; i < threads_count; ++i) {
             workers.emplace_back([this, i] {
                 while (true) {
@@ -36,11 +42,14 @@ public:
                     {
                         unique_lock<mutex> lock(mtx);
 
+                        auto wait_start = chrono::steady_clock::now();
                         cv.wait(lock, [this] {
                             return stop_immediate ||
                                 (stop_graceful && tasks.empty()) ||
                                 (!tasks.empty() && !is_paused);
                             });
+                        auto wait_end = chrono::steady_clock::now();
+                        worker_wait_times[i] += (wait_end - wait_start);
 
                         if (stop_immediate || (stop_graceful && tasks.empty())) {
                             return;
@@ -51,16 +60,18 @@ public:
                         total_queued_time -= task.duration;
                     }
 
-                    cout << "[Thread " << i << "] Processing task #" << task.id << endl;
+                    cout << "[Worker " << i << "] Started Task #" << task.id << " (" << task.duration << "s)\n";
                     task.work();
+                    completed_tasks++;
                 }
-                });
+            });
         }
     }
 
     bool enqueue(int id, int duration) {
         lock_guard<mutex> lock(mtx);
         if (total_queued_time + duration > 60) {
+            rejected_tasks++;
             return false;
         }
         Task new_task;
@@ -100,29 +111,67 @@ public:
         }
     }
 
+    void printMetrics() {
+        cout << "\n--------------- LAB 3 STATA ----------------\n";
+        cout << "Threads in pool: " << workers.size() << "\n";
+        cout << "Completed tasks: " << completed_tasks << "\n";
+        cout << "Rejected tasks (limit > 60s): " << rejected_tasks << "\n";
+
+        double total_wait = 0;
+        for (size_t i = 0; i < worker_wait_times.size(); ++i) {
+            cout << "Worker [" << i << "] vacant time: " << fixed << setprecision(2) << worker_wait_times[i].count() << "s\n";
+            total_wait += worker_wait_times[i].count();
+        }
+        cout << "Average vacant time: " << total_wait / workers.size() << "s\n";
+        cout << "------------------------------------------------\n";
+    }
+
     ~ThreadPool() {
         shutdown(false);
     }
 };
 
-int main() {
-    ThreadPool pool(4);
+void taskProducer(ThreadPool& pool, int producer_id, atomic<bool>& stop) {
+    int counter = 0;
 
-    for (int i = 1; i <= 5; ++i) {
-        if (pool.enqueue(i, 5)) {
-            cout << "Task " << i << " enqueued." << endl;
+    while (!stop) {
+        int duration = rand() % 8 + 5;
+        int task_id = producer_id * 100 + counter++;
+
+        if (pool.enqueue(task_id, duration)) {
+            cout << "[P" << producer_id << "] ADD #" << task_id << " (" << duration << "s)" << endl;
         }
+        else {
+            cout << "[P" << producer_id << "] REJECTED #" << task_id << endl;
+        }
+
+        this_thread::sleep_for(chrono::seconds(rand() % 3 + 1));
     }
+}
 
-    this_thread::sleep_for(chrono::seconds(2));
+int main() {
+    srand(time(0));
+    ThreadPool pool(4);
+    atomic<bool> stop_producing{ false };
+
+    thread p1(taskProducer, ref(pool), 1, ref(stop_producing));
+    thread p2(taskProducer, ref(pool), 2, ref(stop_producing));
+
+    this_thread::sleep_for(chrono::seconds(15));
     pool.pause();
-    cout << "Pool paused... please wait and don't get bored" << endl;
-
-    this_thread::sleep_for(chrono::seconds(3));
+    this_thread::sleep_for(chrono::seconds(7));
     pool.resume();
+    this_thread::sleep_for(chrono::seconds(15));
 
+    cout << "\nStopping producers...\n";
+    stop_producing = true;
+    p1.join();
+    p2.join();
+
+    cout << "Finalizing remaining tasks...\n";
     pool.shutdown(false);
-    cout << "Pool shut down." << endl;
+
+    pool.printMetrics();
 
     return 0;
 }
